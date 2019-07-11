51b1fd6430d6a08ca0d8c931bb71a0c8a85a26c8
caio guilherme <caioguilhermeuna@gmail.com>
2019-07-11 07:55:21 -0300

    template

---
+++ templatepattern/home.ts
@@ -0,0 +1,42 @@
+abstract class CallOfDutyBlackOps4{
+    abstract startpPlay():void;
+    abstract endPlay():void;
+
+    play():void{
+        this.startpPlay();
+        this.endPlay();
+    }
+}
+class Blackout extends CallOfDutyBlackOps4{
+
+    startpPlay(){
+        console.log("O modo de jogo Blackout foi estartado!");
+    }
+    endPlay(){
+        console.log("O modo de jogo Blackout foi finalizado!");
+    }
+}
+class Multiplayer extends CallOfDutyBlackOps4{
+
+    startpPlay(){
+        console.log("O modo de jogo Multiplayer foi estartado!");
+    }
+    endPlay(){
+        console.log("O modo de jogo Multiplayer foi finalizado!");
+    }
+}
+class Zombies extends CallOfDutyBlackOps4{
+
+    startpPlay(){
+        console.log("O modo de jogo Zombies foi estartado!");
+    }
+    endPlay(){
+        console.log("O modo de jogo Zombies foi finalizado!");
+    }
+}
+let blackout = new Blackout();
+let multiplayer = new Multiplayer();
+let zombies = new Zombies();
+blackout.play();
+multiplayer.play();
+zombies.play();
